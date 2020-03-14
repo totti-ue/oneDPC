4c06084503870a7bad816eb119b754e108cef354
totti-ue <osamu232@yahoo.co.jp>
2020-03-15 01:33:30 +0900

    big-changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :today_theme, only: [:index, :show]
 
   def index
     @users = User.where.not(id: current_user.id)
@@ -7,13 +8,28 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @nickname = @user.nickname 
-    @posts = @user.posts.order("created_at DESC").page(params[:page]).per(4)
-    @liked = @user.liked_posts.order("created_at DESC").page(params[:page]).per(4)
+    @posts = @user.posts.order("created_at DESC").page(params[:page]).per(20)
+    @liked = @user.liked_posts.order("created_at DESC").page(params[:page]).per(20)
   end
 
-  def move_to_index  
-    redirect_to action: :index unless user_signed_in?
+
+  private
+
+  # def move_to_index  
+  #   redirect_to action: :index unless user_signed_in?
+  # end
+  
+  def today_theme
+    @today_theme = Theme.find(today_num)
   end
 
+  def today_num
+    if Time.now.wday == 0
+       return 1
+    else
+       return Time.now.day
+    end
+  end
+  
 end
   
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,9 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :liked_posts, through: :likes, source: :post
+
+  validates :nickname, presence: true
+
   def already_liked?(post)
     self.likes.exists?(post_id: post.id)
   end
