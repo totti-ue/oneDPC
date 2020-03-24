473434f2c1a00ea3f7fb82e7b56083099aa19ca9
totti-ue <osamu232@yahoo.co.jp>
2020-03-25 00:26:21 +0900

    implementation_of_user_follow_function
    # what
    フォロー機能の実装
    
    # why
    ユーザー間の関係性を豊かにするため

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,28 @@
+class RelationshipsController < ApplicationController
+  before_action :set_user, only: [:create, :destroy]
+
+  def create
+    following = current_user.follow(@user)
+    if following.save
+      redirect_to @user
+    else
+      redirect_to @user
+    end
+  end
+
+  def destroy
+    following = current_user.unfollow(@user)
+    if following.destroy
+      redirect_to @user
+    else
+      redirect_to @user
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:follow_id])
+  end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,18 +3,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  # userが削除される同時に、userに関連するposts,comments,likesも削除される。
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :liked_posts, through: :likes, source: :post
   has_many :favorites, dependent: :destroy
   has_many :fav_posts, through: :favorites, source: :post
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverse_of_relationships, class_name: 'Relationhip', foreign_key: 'follow_id'
+  has_many :followers, through: :reverse_of_relationships, source: :user
 
   validates :nickname, presence: true
 
   mount_uploader :avator, ImageUploader
-
 
   def already_liked?(post)
     self.likes.exists?(post_id: post.id)
@@ -24,5 +26,19 @@ class User < ApplicationRecord
     self.favorites.exists?(post_id: post.id)
   end
 
+  def follow(other_user)
+    unless self == other_user
+      self.relationships.find_or_create_by(follow_id: other_user.id)
+    end
+  end
+
+  def unfollow(other_user)
+    relationship = self.relationships.find_by(follow_id: other_user.id)
+    relationship.destroy if relationship
+  end
+
+  def following?(other_user)
+    self.followings.include?(other_user)
+  end
 
 end
