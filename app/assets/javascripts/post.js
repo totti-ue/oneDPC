70b7b1471b681ec534b30991f3b17c82798668a1
totti-ue <osamu232@yahoo.co.jp>
2020-04-03 16:54:52 +0900

    create_mobile_new_and_edit_post_page
    # what
    スマホ用の新規投稿および編集画面作成。
    
    # why
    スマホで使いやすくするため。

--- app/assets/javascripts/post.js
+++ app/assets/javascripts/post.js
@@ -7,7 +7,6 @@ $(function() {
             alart("画像ファイルを選択してください");
             return false;
         }
-
         reader.onload = (function(file){
             return function(e){
                 $("#new-img_prev").attr("src", e.target.result);
@@ -15,7 +14,24 @@ $(function() {
             };
         })(file);
         reader.readAsDataURL(file);
-
     });
 });
 
+$(function() {
+    $('#mobile-new-img').change(function(e){
+        let file = e.target.files[0];
+        let reader = new FileReader();
+
+        if(file.type.indexOf("image") < 0){
+            alart("画像ファイルを選択してください");
+            return false;
+        }
+        reader.onload = (function(file){
+            return function(e){
+                $("#mobile-new-img_prev").attr("src", e.target.result);
+                $("#mobile-new-img_prev").attr("title", file.name);
+            };
+        })(file);
+        reader.readAsDataURL(file);
+    });
+});
